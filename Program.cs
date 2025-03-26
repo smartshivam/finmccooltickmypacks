6f379cafb5583fec4e294f2cbc66bca306d47b3d
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-03-26 21:05:32 +0000

    fix

--- Program.cs
+++ Program.cs
@@ -26,17 +26,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    // Password settings
+builder.Services.AddIdentityCore<IdentityUser>(options => {
     options.Password.RequireDigit = false;
     options.Password.RequiredLength = 6;
     options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
 })
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddCors(options =>
 {
