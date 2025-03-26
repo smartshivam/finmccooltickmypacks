4e0dbb8c85c6f4e49a9355e12ad6643ea76a83d2
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-03-26 12:23:27 +0000

    prod

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("https://abkillio.xyz")
               .AllowAnyMethod()   
               .AllowAnyHeader();  
     });
